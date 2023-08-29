12adad34f669ca9c23b94722a502c59ff6d667f9
Alecsander-Cruz <alecsandercruz93@gmail.com>
2023-08-29 18:56:12 -0300

    Commit inicial do projeto spring

---
+++ src/main/java/next/school/cesar/desafiospring/DesafioSpringNExTApplication.java
@@ -0,0 +1,13 @@
+package next.school.cesar.desafiospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringNExTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringNExTApplication.class, args);
+	}
+
+}
