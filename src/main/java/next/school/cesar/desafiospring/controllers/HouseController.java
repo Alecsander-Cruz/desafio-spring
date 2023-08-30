c59a7acb889635eecc583680a6db90034f354d18
Alecsander-Cruz <alecsandercruz93@gmail.com>
2023-08-29 22:34:42 -0300

    Adicionado o esqueleto dos Controllers

---
+++ src/main/java/next/school/cesar/desafiospring/controllers/HouseController.java
@@ -0,0 +1,9 @@
+package next.school.cesar.desafiospring.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/houses")
+public class HouseController {
+}
---
+++ src/main/java/next/school/cesar/desafiospring/controllers/InsuranceController.java
@@ -0,0 +1,9 @@
+package next.school.cesar.desafiospring.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/insurances")
+public class InsuranceController {
+}
